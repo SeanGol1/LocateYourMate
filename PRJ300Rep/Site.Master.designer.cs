f8aae766cd4d42b5d7dd8f9d42965567b9e29769
Sean Golden <seangol1@hotmail.com>
2017-10-10 10:28:14 +0100

    fixes

--- PRJ300Rep/Site.Master.designer.cs
+++ PRJ300Rep/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace PRJ300Rep {
     public partial class SiteMaster {
         
         /// <summary>
+        /// navTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor navTitle;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
